8172cc1522a590bc60f4099a3400b9efaffecb70
Taha lodhi <tahalodhi2006@gmail.com>
2022-12-25 17:37:12 -0800

    Fixed the home page to body instead of title

--- website/auth.py
+++ website/auth.py
@@ -2,9 +2,13 @@ from flask import Blueprint
 
 auth = Blueprint('auth', __name__)
 
+@auth.route("/")
+def home():
+    return "<p>home</p>"
+    
 @auth.route("/login")
 def login():
     return "<p>login</p>"
-    
+
 
 
